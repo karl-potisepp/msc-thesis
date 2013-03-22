8b9c3d2d44e4b2ff2a17d706fb32af834b7590d2
karl <you@example.com>
2013-03-22 12:20:10 +0200

    removed unused code

--- code/src/ee/ut/sci/potisepp/HadoopBFChaudhury.java
+++ code/src/ee/ut/sci/potisepp/HadoopBFChaudhury.java
@@ -33,8 +33,8 @@ public class HadoopBFChaudhury {
 			
 			//extract info from the key
 			String[] p = key.toString().split(";");
-			int width = Integer.valueOf(p[1]);
-			int height = Integer.valueOf(p[2]);
+//			int width = Integer.valueOf(p[1]);
+//			int height = Integer.valueOf(p[2]);
 			int channels = Integer.valueOf(p[3]);
 			int bpp = Integer.valueOf(p[4]);
 //			float min = Float.parseFloat(p[5]);
--- code/src/ee/ut/sci/potisepp/MyImageReader.java
+++ code/src/ee/ut/sci/potisepp/MyImageReader.java
@@ -41,46 +41,19 @@ public class MyImageReader implements RecordReader<Text, Data>{
 		FSDataInputStream fsin = fs.open(path);
 		img = ImageIO.read(fsin);
 		
-//		int width = img.getWidth();
-//		int height = img.getHeight();
-//		int channels = img.getColorModel().getNumColorComponents();
 		int bpp = img.getColorModel().getPixelSize();
-//		int pixels = width * height;
-//		float min = Float.MAX_VALUE, max = Float.MIN_VALUE;
-//		
-////		byte[] values = new byte[width * height * channels];
-//		float[][] values = new float[channels][width * height];
-//		
-//		//place the data from BufferedImage into a BytesWritable
-//		float progressIncrement = 1.0f / pixels;
-//		float[] rgb;
-//		for (int x = 0; x < width; x++){
-//			for(int y = 0; y < height; y++){
-//				rgb = getPixelData(img.getRGB(x, y));
-//				for (int k = 0; k < rgb.length; k++){
-//					values[k][x + y*width] = rgb[k];
-//					
-//					if (rgb[k] > max) max = rgb[k];
-//					if (rgb[k] < min) min = rgb[k];
-//					
-//					counter++;
-//				}
-//				progress+=progressIncrement;
-//			}
-//		}
 		
 		ImagePlus imp = new ImagePlus("", img);
 		value = new Data(imp);
 		
-		//set key to the form of "FILENAME.EXTENSION;WIDTH_PIXELS;HEIGHT_PIXELS;NO_CHANNELS//;BITS_PER_PIXEL;MIN;MAX"
+		//set key to the form of "FILENAME.EXTENSION;WIDTH_PIXELS;HEIGHT_PIXELS;NO_CHANNELS
 		String filename = path.toString();
 		String sizeinfo = "";
 		sizeinfo += ";" + String.valueOf(value.nx);
 		sizeinfo += ";" + String.valueOf(value.ny);
 		sizeinfo += ";" + String.valueOf(value.nc);
 		sizeinfo += ";" + String.valueOf(bpp);
-//		sizeinfo += ";" + String.valueOf(min);
-//		sizeinfo += ";" + String.valueOf(max);
+		
 		int lastSlash = filename.lastIndexOf("/");
 		if( lastSlash > 0 && lastSlash+1 < filename.length()){
 			key.set(filename.substring(lastSlash+1) + sizeinfo);
@@ -89,23 +62,9 @@ public class MyImageReader implements RecordReader<Text, Data>{
 		}
 		
 		
-		
-//		System.out.println("READ IMAGE: " + key.toString() + " PIXELS: " + (counter/3));
-		
 		fsin.close();
 		
 	}
-	
-//	private static float[] getPixelData(int argb) {
-//		int rgb[] = new int[]{
-//				//figure this out
-//				(argb >> 16) & 0xff,
-//				(argb >> 8) & 0xff,
-//				(argb ) & 0xff
-//		};
-////		System.out.println((byte) rgb[0] + " " + (byte) rgb[1] + " " + (byte) rgb[2]);
-//		return new float[]{ (float) rgb[0] , (float) rgb[1], (float) rgb[2]};
-//	}
 	
 	public void initialize(InputSplit inputSplit, TaskAttemptContext taskAttemptContext) throws IOException, InterruptedException {
 //		System.out.println("initialize");
